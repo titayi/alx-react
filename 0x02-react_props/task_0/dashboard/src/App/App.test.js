539d2f23e2e1206bc95cda468070c8e039be2864
titayi <kitayimike@gmail.com>
2023-11-07 15:58:10 +0300

    Test files

--- 0x02-react_props/task_0/dashboard/src/App/App.test.js
+++ 0x02-react_props/task_0/dashboard/src/App/App.test.js
@@ -1,7 +1,38 @@
-import App from "./App";
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+import Login from '../Login/Login';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import Notifications from '../Notifications/Notifications';
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App tests', () => {
+  it('renders without crashing', () => {
+    const { container } = render(<App />);
+    expect(container).toBeDefined();
+  });
+
+  it('should render Notifications component', () => {
+    const { getByText } = render(<App />);
+    const notifications = getByText('Notifications'); 
+    expect(notifications).toBeInTheDocument();
+  });
+
+  it('should render Header component', () => {
+    const { getByText } = render(<App />);
+    const header = getByText('Your Header Text');
+    expect(header).toBeInTheDocument();
+  });
+
+  it('should render Login Component', () => {
+    const { getByText } = render(<App />);
+    const login = getByText('Login');
+    expect(login).toBeInTheDocument();
+  });
+
+  it('should render Footer component', () => {
+    const { getByText } = render(<App />);
+    const footer = getByText('Copyright');
+    expect(footer).toBeInTheDocument();
+  });
 });
